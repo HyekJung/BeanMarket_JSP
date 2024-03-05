e011a94220330c9a196aa2a12bdd9547c974e4c7
HyekJung <155180714+HyekJung@users.noreply.github.com>
2024-03-05 09:28:31 +0900

    Add files via upload

--- src/main/java/profile_p/ProfileDelete.java
+++ src/main/java/profile_p/ProfileDelete.java
@@ -1,19 +1,24 @@
-package profile_con;
+package profile_p;
 
+import dao_p.ProfileDAO;
+import dto_p.MemberDTO;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import service_p.ProfileService;
 
 public class ProfileDelete implements ProfileService {
 
 	@Override
 	public void execute(HttpServletRequest request, HttpServletResponse response) {
-		String userId = request.getParameter("userId");
+		MemberDTO dto = (MemberDTO)request.getSession().getAttribute("sessDto");
 		
-		new ProfileDAO().delete(userId);
-		
-		request.setAttribute("mainUrl", "ProfileDelete.jsp");
+		new ProfileDAO().delete(dto.getUserId());
+		HttpSession session = request.getSession();
+		session.invalidate();
+		request.setAttribute("mainUrl", "inc/alert.jsp");
 		request.setAttribute("msg", "회원 탈퇴되었습니다.");
-		request.setAttribute("goUrl", "main/main");
+		request.setAttribute("goUrl", "/firstProj/main/Main");
 		
 	}
 	
--- src/main/java/profile_p/ProfileDetail.java
+++ src/main/java/profile_p/ProfileDetail.java
@@ -1,18 +1,16 @@
-package profile_con;
-
-import java.util.ArrayList;
+package profile_p;
 
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import service_p.ProfileService;
 
 
 public class ProfileDetail implements ProfileService {
 
 	@Override
 	public void execute(HttpServletRequest request, HttpServletResponse response) {
-		ArrayList<ProfileDTO> data = new ProfileDAO().profileList();
-		request.setAttribute("mainData", data);
+		
 		System.out.println("ProfileDetail.execute() 실행 : ");
 	}
 
